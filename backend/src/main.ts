bde93bdc0226a3362e722dafeb440fc593511116
nhitar <nikita.pivoev@yandex.ru>
2026-02-22 19:53:51 +0300

    feat: add configs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,9 @@ import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  app.enableCors({origin: '*'});
-  
+
+  app.enableCors({ origin: '*' });
+
   await app.listen(3000);
   console.log('Server running on http://localhost:3000');
 }
