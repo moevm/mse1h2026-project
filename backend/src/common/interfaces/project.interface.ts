88c29635f21ae4f3069de82d6cbda250dcbebd4a
Никита Пивоев <nikita.pivoev@yandex.ru>
2026-03-01 07:05:03 +0300

    refactor: lint & formatter

--- backend/src/common/interfaces/project.interface.ts
+++ backend/src/common/interfaces/project.interface.ts
@@ -3,9 +3,9 @@ export interface Project {
   title: string;
   description?: string;
   teacherId: number;
-  teacherFirstName: string,
-  teacherLastName: string,
-  courseName: string,
+  teacherFirstName: string;
+  teacherLastName: string;
+  courseName: string;
   courseId: number;
   createdAt?: Date;
   updatedAt?: Date;
