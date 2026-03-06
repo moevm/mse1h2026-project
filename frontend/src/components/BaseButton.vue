56211bce8635d51cc7210a4ee32a19417a641a06
katerina2121 <katya.lobova2005@gmail.com>
2026-03-06 21:05:15 +0300

    feat: add component list of courses

---
+++ frontend/src/components/BaseButton.vue
@@ -0,0 +1,43 @@
+<!-- BaseButton.vue -->
+<template>
+  <button
+    :class="['base-btn', btnClass]"
+    v-bind="$attrs"
+    @click="$emit('click', $event)"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  text: string;
+  btnClass?: string;
+}>();
+
+defineEmits<{
+  (e: 'click', event: MouseEvent): void;
+}>();
+</script>
+
+<style scoped>
+.base-btn {
+  padding: 6px 12px;
+  border: none;
+  border-radius: 6px;
+  font-size: 0.9rem;
+  cursor: pointer;
+  transition: all 0.2s;
+  font-size: 1rem;
+}
+
+.base-btn:hover {
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
+}
+
+.confirm-btn {
+    background-color: red;
+    color: white;
+}
+
+</style>
