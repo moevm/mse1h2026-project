c86e8cdba24e0bbc6145699d533fcec3dc5fce01
nhitar <nikita.pivoev@yandex.ru>
2026-03-07 16:02:43 +0300

    fix: backend cors allows frontend and .env works from root

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:80', 'http://localhost:5173'],
+    origin: ['http://127.0.0.1:80', 'http://127.0.0.1:5173'],
   });
 
   await app.listen(3000);
