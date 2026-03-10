156be3e9a4205f1c10e950367672c318d2e99902
Никита Пивоев <nikita.pivoev@yandex.ru>
2026-03-10 22:55:25 +0300

    fix: commit hooks

--- backend/src/common/guards/auth.guard.ts
+++ backend/src/common/guards/auth.guard.ts
@@ -1,4 +1,10 @@
-import { CanActivate, ExecutionContext, Injectable, SetMetadata, UnauthorizedException } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  SetMetadata,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
