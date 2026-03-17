3676765df09631ba3c6bf3ab5b6abbaca665d286
KorzikAlex <lumic.alex@gmail.com>
2026-03-17 21:51:08 +0300

    fix index.ts

--- database/src/index.ts
+++ database/src/index.ts
@@ -1,9 +1,1 @@
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
-import { PrismaClient } from '../generated/prisma/client';
-import { getDatabaseUrl } from './env';
-
-const adapter = new PrismaMariaDb(getDatabaseUrl());
-
-export const prisma = new PrismaClient({ adapter });
-
 export * from '../generated/prisma/client';
