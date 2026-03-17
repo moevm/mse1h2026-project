a9cd85b85e43063c09c47dec6da2468fa5684524
KorzikAlex <lumic.alex@gmail.com>
2026-03-17 21:53:26 +0300

    update hello_world

--- database/src/scripts/hello_world.ts
+++ database/src/scripts/hello_world.ts
@@ -1,28 +1,10 @@
 import 'dotenv/config';
-import { prisma } from '../index';
 
 async function main(): Promise<void> {
-  const createdUser = await prisma.user.create({
-    data: {
-      firstName: 'Test',
-      secondName: 'User',
-      email: 'test@example.com',
-      groupNumber: 1,
-      role: 'student',
-    },
-  });
-
-  console.log('Created user:', createdUser);
-
-  await prisma.user.delete({ where: { id: createdUser.id } });
-  console.log('Success');
+  console.log('Hello, World!');
 }
 
-void main()
-  .catch((error: unknown) => {
-    console.error(error);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+void main().catch((error: unknown) => {
+  console.error(error);
+  process.exit(1);
+});
