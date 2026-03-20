8095645c48b49d57719550fc2a5f4724df625e8c
polina <polinaa.gelman@gmail.com>
2026-03-20 18:51:56 +0300

    fix: CORS frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: ['http://127.0.0.1:80', 'http://127.0.0.1:5173'],
+    origin: ['http://127.0.0.1:80', 'http://127.0.0.1:5173', 'http://localhost:5173/'],
   });
 
   await app.listen(3000);
