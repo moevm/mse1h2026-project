098f46306a24a1a175149ce31e4f3afc7bbc00a6
KorzikAlex <lumic.alex@gmail.com>
2026-04-16 12:29:48 +0300

    prettier fix

--- backend/src/ldap/ldap.module.ts
+++ backend/src/ldap/ldap.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+
 import { LdapService } from './ldap.service';
 
 @Module({
-  providers: [LdapService]
+  providers: [LdapService],
 })
 export class LdapModule {}
