87768eb1547f7479a89932eaff8363bbe2f68271
KorzikAlex <lumic.alex@gmail.com>
2026-05-04 08:21:42 +0300

    Добавлены заглушки тестов

---
+++ backend/src/teams/teams.controller.spec.ts
@@ -0,0 +1,22 @@
+import { PrismaService } from '@/prisma/prisma.service';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { TeamsController } from './teams.controller';
+import { TeamsService } from './teams.service';
+
+describe('TeamsController', () => {
+  let controller: TeamsController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [TeamsController],
+      providers: [PrismaService, TeamsService],
+    }).compile();
+
+    controller = module.get<TeamsController>(TeamsController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
---
+++ backend/src/teams/teams.service.spec.ts
@@ -0,0 +1,40 @@
+import { PrismaService } from '@/prisma/prisma.service';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { TeamsService } from './teams.service';
+
+jest.mock('@/prisma/prisma.service', () => {
+  return {
+    PrismaService: jest.fn()
+  };
+});
+
+describe('TeamsService', () => {
+  let service: TeamsService;
+
+  beforeAll(async () => {});
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [TeamsService, PrismaService],
+    }).compile();
+
+    service = module.get<TeamsService>(TeamsService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should get team by id', async () => {
+
+  });
+
+  it('should create invitation', async () => {});
+
+
+
+  afterEach(async () => {});
+
+  afterAll(async () => {});
+});
