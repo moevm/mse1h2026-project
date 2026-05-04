9ed882a1227f41678f50575c0d0b073b01046559
Danil Otmakhov <danil.otmakhov@gmail.com>
2026-05-05 01:17:30 +0300

    feat(backend): add my invitations endpoint

--- backend/src/invitations/invitations.controller.ts
+++ backend/src/invitations/invitations.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put, Req } from '@nestjs/common';
 
 import { UpdateInvitationDto } from './dto/update-invitation.dto';
 import { InvitationsService } from './invitations.service';
@@ -6,6 +6,11 @@ import { InvitationsService } from './invitations.service';
 @Controller('api/invitations')
 export class InvitationsController {
   constructor(private readonly invitationsService: InvitationsService) {}
+
+  @Get('my')
+  getMyInvitations(@Req() req) {
+    return this.invitationsService.getMyInvitations(req.user.sub);
+  }
 
   @Put(':id')
   updateInvitation(@Param('id') id: string, @Body() updateInvitationDto: UpdateInvitationDto) {
