13fb92eacf71df1d2ac2438bc301f2ee32dd547f
KorzikAlex <lumic.alex@gmail.com>
2026-05-24 22:48:11 +0300

    Пока отключаем модуль ldap

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,6 +11,7 @@ import { ExchangesModule } from './exchanges/exchanges.module';
 import { HealthModule } from './health/health.module';
 import { HelloWorldModule } from './hello-world/hello-world.module';
 import { InvitationsModule } from './invitations/invitations.module';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { LdapModule } from './ldap/ldap.module';
 import { ProjectsModule } from './projects/projects.module';
 import { TeamsModule } from './teams/teams.module';
@@ -34,7 +35,7 @@ import { UsersModule } from './users/users.module';
     InvitationsModule,
     HealthModule,
     ExchangesModule,
-    LdapModule,
+    //LdapModule,
   ],
 })
 export class AppModule {}
